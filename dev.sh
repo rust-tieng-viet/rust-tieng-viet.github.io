9d1950dfbfb69247d98075a78a0f222008b433c7
Duyet Le <me@duyet.net>
2024-03-13 21:41:41 +0700

    chore(deps): add linkcheck

--- dev.sh
+++ dev.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+PORT=${1:-3000}
+
 if ! command -v mdbook &>/dev/null; then
   echo Installing mdBook https://rust-lang.github.io/mdBook/guide/installation.html ...
   cargo install mdbook
 fi
 
-mdbook serve --open
+if ! command -v mdbook-linkcheck &>/dev/null; then
+  echo Installing mdbook-linkcheck
+  cargo install mdbook-linkcheck
+fi
+
+mdbook serve --open --port $PORT
