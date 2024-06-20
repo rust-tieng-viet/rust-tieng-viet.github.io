8d597d5b828d7c30793175016d0564d9854da75c
duyet <5009534+duyet@users.noreply.github.com>
2024-06-20 09:48:41 +0000

    Deploying to gh-pages from @ rust-tieng-viet/rust-tieng-viet.github.io@05ab6384e5d52ab63fe98a25bdd6312d3cdc7469 🚀

---
+++ crates/tokio-example/src/main.rs
@@ -0,0 +1,28 @@
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // Respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
+}
